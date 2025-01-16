a3b317e1eaac1c60a28be2027c9abc072a1b65b1
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-01-15 21:51:08 -0800

    adding some gridlines

--- day23/main.py
+++ day23/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen,Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -7,8 +7,21 @@ from scoreboard import Scoreboard
 traffic = CarManager()
 player = Player()
 screen = Screen()
+grid_lines = Turtle()
+
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+for i in range(-300,310,10):
+    if i %50 == 0:
+        grid_lines.pencolor('red')
+    else:
+        grid_lines.pencolor('black')
+    grid_lines.pu()
+    grid_lines.goto(-300,i)
+    grid_lines.pd()
+    grid_lines.fd(600)
+
 
 game_is_on = True
 while game_is_on:
