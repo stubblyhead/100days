d4bceebb31c23067ccbfe4e4c3c77bbc85c33d69
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-01-23 19:46:54 -0800

    day 25 done

--- day25/main.py
+++ day25/main.py
@@ -11,19 +11,25 @@ print(state_dict)
 screen = Screen()
 screen.title('us states game')
 screen.bgpic('blank_states_img.gif')
-
 answer = Turtle()
 answer.hideturtle()
 answer.pu()
-
+dumbass = False
 while state_dict:
-    guess = textinput(prompt='Guess a state',title='your guess')
+    guess = textinput(prompt='Guess a state',title='your guess').title()
     if guess in state_dict.keys():
         answer.goto(state_dict[guess])
         answer.write(guess, align='center', font=['Arial', 8, 'normal'])
         state_dict.pop(guess)
     else:
-        print('you dumbass')
+        dumbass = True
         break
+
+answer.goto(0,-240)
+if dumbass:
+    answer.color('red')
+    answer.write('you dumbass', align='center', font=['Arial', 24, 'bold'])
+else:
+    answer.write('good job', align='center', font=['Arial', 24, 'bold'])
 
 screen.exitonclick()
