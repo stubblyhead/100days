72f9001ea929ea69c2d6356e684969d7361f39b9
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-02-13 10:13:12 -0800

    tomato time

--- day28/main.py
+++ day28/main.py
@@ -28,8 +28,16 @@ bg_image = tkinter.PhotoImage(file='tomato.png')
 canvas = tkinter.Canvas(width=210,height=224,bg=YELLOW,highlightthickness=0)
 canvas.create_image(106,112, image=bg_image)
 canvas.create_text(106,140,text='00:00',fill="white",font=(FONT_NAME,24,'bold'))
-canvas.pack()
+canvas.grid(row=2,column=2)
 
+title = tkinter.Label(text="Timer",fg=GREEN, bg=YELLOW,font=(FONT_NAME,48,'normal'))
+title.grid(row=1,column=2)
 
+start_btn = tkinter.Button(text="Start")
+reset_btn = tkinter.Button(text="Reset")
+start_btn.grid(row=3,column=1)
+reset_btn.grid(row=3,column=3)
 
+count = tkinter.Label(text="✓",fg=GREEN,bg=YELLOW,font=(FONT_NAME,24,"bold"))
+count.grid(row=4,column=2)
 window.mainloop()
