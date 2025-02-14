7916945922ba31680ab9d920099277d46e8709f3
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-02-13 19:10:44 -0800

    pyformat.info for great good

--- day28/main.py
+++ day28/main.py
@@ -13,8 +13,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_cmd():
+    countdown(300)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_string = "{0:02d}:{1:02d}".format(count//60,count%60)
+    canvas.itemconfig(timer_text, text=count_string)
+    if count > 0:
+        window.after(1000, countdown, count-1)
+        
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
@@ -27,13 +35,13 @@ bg_image = tkinter.PhotoImage(file='tomato.png')
 
 canvas = tkinter.Canvas(width=210,height=224,bg=YELLOW,highlightthickness=0)
 canvas.create_image(106,112, image=bg_image)
-canvas.create_text(106,140,text='00:00',fill="white",font=(FONT_NAME,24,'bold'))
+timer_text = canvas.create_text(106,140,text='00:00',fill="white",font=(FONT_NAME,24,'bold'))
 canvas.grid(row=2,column=2)
 
 title = tkinter.Label(text="Timer",fg=GREEN, bg=YELLOW,font=(FONT_NAME,48,'normal'))
 title.grid(row=1,column=2)
 
-start_btn = tkinter.Button(text="Start")
+start_btn = tkinter.Button(text="Start", command=start_cmd)
 reset_btn = tkinter.Button(text="Reset")
 start_btn.grid(row=3,column=1)
 reset_btn.grid(row=3,column=3)
