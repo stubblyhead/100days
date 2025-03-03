c233440f8b2fa424faeef4ad4ba698c96d119f67
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-03-03 15:13:40 -0800

    tomato time

--- day28/main.py
+++ day28/main.py
@@ -9,18 +9,56 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TESTING = True
+CHECKMARK = "✓"
+reps = 0
+checklabel = ''
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_cmd():
+    global reps
+    global checklabel
+    global count
+    global timer
+    window.after_cancel(timer)
+    checklabel = ''
+    count.config(text=checklabel)
+    canvas.itemconfig(timer_text, text="00:00")
+    title.config(text='')
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_cmd():
-    countdown(300)
+    global reps
+    
+    reps += 1
+    if TESTING:
+        factor = 1
+    else:
+        factor = 60
+    if reps % 2 == 1:
+        title.config(text='Work', fg=GREEN)
+        countdown(WORK_MIN * factor)
+    elif reps % 8 == 0:
+        title.config(text='Break', fg=RED)
+        countdown(LONG_BREAK_MIN * factor)
+    elif reps % 2 == 0:
+        title.config(text='Break', fg=PINK)
+        countdown(SHORT_BREAK_MIN * factor)
+    
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def countdown(count):
-    count_string = "{0:02d}:{1:02d}".format(count//60,count%60)
+def countdown(sec):
+    global timer
+    global reps
+    #global count
+    global checklabel
+    count_string = "{0:02d}:{1:02d}".format(sec//60,sec%60)
     canvas.itemconfig(timer_text, text=count_string)
-    if count > 0:
-        window.after(1000, countdown, count-1)
+    if sec > 0:
+        timer = window.after(1000, countdown, sec-1)
+    if sec == 0 and reps % 2 == 0:
+        checklabel += CHECKMARK
+        count.config(text=checklabel) 
         
 
 
@@ -38,14 +76,14 @@ canvas.create_image(106,112, image=bg_image)
 timer_text = canvas.create_text(106,140,text='00:00',fill="white",font=(FONT_NAME,24,'bold'))
 canvas.grid(row=2,column=2)
 
-title = tkinter.Label(text="Timer",fg=GREEN, bg=YELLOW,font=(FONT_NAME,48,'normal'))
+title = tkinter.Label(text='',fg=GREEN, bg=YELLOW,font=(FONT_NAME,48,'normal'))
 title.grid(row=1,column=2)
 
 start_btn = tkinter.Button(text="Start", command=start_cmd)
-reset_btn = tkinter.Button(text="Reset")
+reset_btn = tkinter.Button(text="Reset", command=reset_cmd)
 start_btn.grid(row=3,column=1)
 reset_btn.grid(row=3,column=3)
 
-count = tkinter.Label(text="✓",fg=GREEN,bg=YELLOW,font=(FONT_NAME,24,"bold"))
+count = tkinter.Label(text=checklabel,fg=GREEN,bg=YELLOW,font=(FONT_NAME,24,"bold"))
 count.grid(row=4,column=2)
 window.mainloop()
