8b10695dacd879a1909b0846b2c9f88af34d9d94
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-03-03 16:14:09 -0800

    add focus to first entry field

--- day29/main.py
+++ day29/main.py
@@ -12,7 +12,7 @@ import tkinter
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=f0, pady=20)
 
 bg_image = tkinter.PhotoImage(file='logo.png')
 
@@ -24,6 +24,7 @@ website_lbl = tkinter.Label(text='Website:')
 website_lbl.grid(row=1,column=0)
 website_entry = tkinter.Entry(width=35)
 website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
 
 username_lbl = tkinter.Label(text='Email/username:')
 username_lbl.grid(row=2,column=0)
