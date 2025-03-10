5caeacaba404a8f57e8bfff53d5171a43c3fd644
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-03-10 15:41:27 -0700

    happy birthday to me

--- day32/happybirthday.py
+++ day32/happybirthday.py
@@ -1,13 +1,14 @@
 import smtplib
 import datetime as dt
+from random import choice
 
 with open('totallynotpasswords.txt') as data:
     line = data.read()
     email,pw = line.split(',')
 
 with open('birthdays.csv') as data:
-    _ = data.readline()
-    people = list(map(lambda s: s.split(','), data.readlines()))
+    _ = data.readline() # throw out headers
+    people = list(map(lambda s: s.strip().split(','), data.readlines()))
 
 letters = []
 for i in range(1,4):
@@ -15,14 +16,18 @@ for i in range(1,4):
         letters.append(data.read())
 
 now = dt.datetime.now()
+cur_month = str(now.month)
+cur_day = str(now.day)
 
-
-
-with smtplib.SMTP("smtp.gmail.com", port=587) as cxn:
-    cxn.starttls()
-    cxn.login(user=email,password=pw)
-    cxn.sendmail(
-        from_addr=email,
-        to_addrs=dest_email,
-        msg="Subject:hello thar\n\nthis is an email"
-    )
+for p in people:
+    if cur_month == p[3] and cur_day == p[4]:
+        letter = choice(letters)
+        letter = letter.replace('[NAME]',p[0])
+        with smtplib.SMTP("smtp.gmail.com", port=587) as cxn:
+            cxn.starttls()
+            cxn.login(user=email,password=pw)
+            cxn.sendmail(
+                from_addr=email,
+                to_addrs=p[1],
+                msg=f"Subject:happy birthday!\n\n{letter}"
+            )
