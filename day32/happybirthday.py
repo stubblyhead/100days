8b8f2324d9652ad21743bb806271f21e8138b538
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-03-10 14:43:07 -0700

    day32 stuff

--- day32/happybirthday.py
+++ day32/happybirthday.py
@@ -1,10 +1,22 @@
 import smtplib
+import datetime as dt
 
 with open('totallynotpasswords.txt') as data:
     line = data.read()
     email,pw = line.split(',')
 
-dest_email = email.replace('@', '+test@')
+with open('birthdays.csv') as data:
+    _ = data.readline()
+    people = list(map(lambda s: s.split(','), data.readlines()))
+
+letters = []
+for i in range(1,4):
+    with open(f'letter_templates/letter_{i}.txt') as data:
+        letters.append(data.read())
+
+now = dt.datetime.now()
+
+
 
 with smtplib.SMTP("smtp.gmail.com", port=587) as cxn:
     cxn.starttls()
