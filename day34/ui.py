a0f20cf97813a95965e7343fba94f30f5ca1b07d
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-03-16 11:30:19 -0700

    day 34 initial commit

---
+++ day34/ui.py
@@ -0,0 +1,11 @@
+import tkinter
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.title('Quizzler')
+        
+
+        self.window.mainloop()
