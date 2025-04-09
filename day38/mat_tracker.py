1780f95b8384ca4c4df6e9537056bac059661744
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-04-08 19:06:02 -0700

    wow that api is kind of a pain in the ass

--- day38/mat_tracker.py
+++ day38/mat_tracker.py
@@ -1,20 +1,39 @@
 import requests
+import datetime
+import json
 
 with open('totallynotpasswords.txt') as f:
-    APP_ID, API_KEY = f.readline().split()
+    APP_ID, API_KEY = f.readline().strip().split()
+    SHEETY_KEY = f.readline().strip()
 
 headers = {
     'x-app-id': APP_ID,
     'x-app-key': API_KEY,
-    'x-remote-user-id': ''
-    ''
+    'x-remote-user-id': '0'
 }
 
-endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+nutritionix_endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+sheety_endpoint = f'https://api.sheety.co/{SHEETY_KEY}/myWorkouts/workouts'
 query = input('what did you do?  ')
-data = {'query': query, 'weight_kg': 90, 'height_cm': 183, 'age': 45}
+ntx_data = {'query': query, 'weight_kg': 90, 'height_cm': 183, 'age': 45}
 
-response = requests.post(url=endpoint, headers=headers, data=data)
+response = requests.post(url=nutritionix_endpoint, headers=headers, data=ntx_data)
 
 response.raise_for_status()
-print(response.json()['exercises'])
+exercises = []
+for e in response.json()['exercises']:
+    exercises.append([e['name'],e['duration_min'],e['nf_calories']])
+
+now = datetime.datetime.today()
+date = now.strftime('%m/%d/%Y')
+time = now.strftime('%H:%M')
+
+headers = {'Content-Type': 'application/json'}
+for e in exercises:
+    name,duration,cals = e
+    hr = duration // 60
+    min = duration % 60
+    duration = f'{hr}:{min}'
+    shy_data = {'workout': {'date': date, 'time': time, 'exercise': name, 'duration': duration, 'calories': cals}}
+    shy_data = json.dumps(shy_data)
+    requests.post(url=sheety_endpoint, data=shy_data, headers=headers)
