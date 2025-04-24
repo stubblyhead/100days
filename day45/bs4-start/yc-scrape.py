9a162e41a9b152249b39379c4c5dcdbc03ca1b33
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-04-23 19:04:50 -0700

    scrape scrape

--- day45/bs4-start/yc-scrape.py
+++ day45/bs4-start/yc-scrape.py
@@ -6,9 +6,18 @@ response.raise_for_status()
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-title_lines = soup.find_all(name='span', class_='titleline')
+title_lines = soup.find_all(name='tr', class_='athing submission')
+max_score = [0, '', '']
 
 for line in title_lines:
-    a_tag = line.find(name='a')
-    print(a_tag.text)
-    print(a_tag.get('href') + '\n')
+    span = line.select('span a')
+    article_id = line.get('id')
+    a_tag = span[0]
+    article_title = a_tag.text
+    article_url = a_tag.get('href')
+    score_line = soup.find(name='span', id=f'score_{article_id}')
+    score = int(score_line.text.split()[0])
+    if score > max_score[0]:
+        max_score = [score, article_title, article_url]
+
+print(max_score)
