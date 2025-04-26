2d76e7d4e87effddb5dad8712a6ea880d2da441d
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-04-26 12:40:10 -0700

    some interesting edge cases here

--- day46/billboard.py
+++ day46/billboard.py
@@ -12,8 +12,8 @@ os.environ['SPOTIPY_CLIENT_ID'] = SPOTIPY_CLIENT_ID
 os.environ['SPOTIPY_CLIENT_SECRET'] = SPOTIPY_CLIENT_SECRET
 os.environ['SPOTIPY_REDIRECT_URI'] = SPOTIPY_REDIRECT_URI
 
-# date = input('What day to go back to? (YYYY-MM-DD) ? ')
-date = '2020-01-01'
+date = input('What day to go back to? (YYYY-MM-DD) ? ')
+# date = '2000-01-01'
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
 response = requests.get(f'https://www.billboard.com/charts/hot-100/{date}/',headers=header)
@@ -24,9 +24,28 @@ chart = soup.find_all(name='div', class_='o-chart-results-list-row-container')
 
 songs = []
 for c in chart:
-    songs.append(c.find(name='h3', id='title-of-a-story').get_text().strip())
+    track = c.find(name='h3', id='title-of-a-story').get_text().strip()
+    artist = c.select('li ul li span')[0].get_text().strip()
+    songs.append([track,artist])
 
-scope = 'user-library-read'
-lz_id = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+scope = 'playlist-modify-private'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+user = sp.current_user()['id']
+sp_tracks = []
+for s in songs:
+    short_title = s[0].split('/')[0] # choose first track from a double-A side single
+    short_title = short_title.split('(')[0] # sometimes chart includes parentheticals not included in spotify titles
+    short_artist = s[1].split()[0] # first word of artist name is usually enough to nail down the right track
+    trackresults = sp.search(q=f'track:{short_title} artist:{short_artist}',type='track')
+    try:
+        sp_tracks.append(trackresults['tracks']['items'][0]['uri'])
+    except IndexError:
+        print(f'Cant find "{s[0]}" by {s[1]}, should be at position {songs.index(s)}')
 
+new_playlist = sp.user_playlist_create(user=user,name=f'Billboard Hot 100 for {date}',public=False)
+
+
+sp.playlist_add_items(playlist_id=new_playlist['id'], items=sp_tracks)
+
+print(new_playlist['external_urls']['spotify'])
