e29cea23c74254ff4108788cf7904cda3f7f6537
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-05-04 18:37:36 -0700

    damn captcha

--- day48/selenium-test.py
+++ day48/selenium-test.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-drv = webdriver.Chrome()
-drv.get('https://www.amazon.com/dp/B075CYMYK6')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+
+drv = webdriver.Chrome(options=chrome_options)
+drv.get('https://www.amazon.com/dp/B075CYMYK6?th=1')
+captcha = drv.find_element(By.LINK_TEXT, "Try different image")
+captcha.click()
 
 price_whole = drv.find_element(By.CLASS_NAME, 'a-price-whole')
 price_fraction = drv.find_element(By.CLASS_NAME, 'a-price-fraction')
 
-print(f'${price_whole}.{price_fraction}')
+print(f'${price_whole.text}.{price_fraction.text}')
