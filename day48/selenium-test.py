49a53bd930412bb3db7cf3e7b571c74c58b9ceb4
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-05-04 17:49:48 -0700

    selenium element selecting

--- day48/selenium-test.py
+++ day48/selenium-test.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 drv = webdriver.Chrome()
-drv.get('https://www.amazon.com')
+drv.get('https://www.amazon.com/dp/B075CYMYK6')
+
+price_whole = drv.find_element(By.CLASS_NAME, 'a-price-whole')
+price_fraction = drv.find_element(By.CLASS_NAME, 'a-price-fraction')
+
+print(f'${price_whole}.{price_fraction}')
