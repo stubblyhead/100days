8563858672de5884e71ad160bd19c195e8e78c77
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-05-05 19:18:33 -0700

    sometimes as high as 175.6, but with a lot of variability

--- day48/cookies.py
+++ day48/cookies.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+cycle_length = 15   
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
@@ -20,10 +21,21 @@ big_cookie = drv.find_element(By.ID, 'bigCookie')
 
 
 while time.time() < big_timeout:
-    short_timeout = time.time() + 15
+    short_timeout = time.time() + cycle_length
     while time.time() < short_timeout:
         big_cookie.click()
     # cookie_count = int(drv.find_element(By.ID, 'cookies').text.split()[0])
+    try:
+        upgrades = drv.find_element(By.ID, 'upgrades')
+        upgrades = upgrades.find_elements(By.CLASS_NAME, 'enabled')
+        upgrades.reverse()
+    except Exception:
+        pass
+    for u in upgrades:
+        try:
+            u.click()
+        except Exception:
+            pass
     buildings = drv.find_element(By.ID, 'products')
     buildings = drv.find_elements(By.CLASS_NAME,'unlocked')
     building_list = []
@@ -32,15 +44,16 @@ while time.time() < big_timeout:
         cur_price = int(buildings[i].find_element(By.ID, f'productPrice{i}').text.replace(',','_'))
 
         building_list.append([cur_price, buildings[i]])
-        building_list.sort()
-        building_list.reverse()
+    building_list.sort()
+    building_list.reverse()
     for b in building_list:
         b[1].click()
+    
 try:
     cps = drv.find_element(By.ID, 'cookiesPerSecond').text
 except Exception:
     cps = drv.find_element(By.ID, 'cookiesPerSecond').text
-print(f'{cps.split(': ')[1]} cookies/second')
+print(f'\n\n{cycle_length} sec cycles\n{cps.split(': ')[1]} cookies/second')
 
     # focus on buildings for now, upgrade prices are kind of obscured
     # upgrades = drv.find_elements(By.ID, 'upgrades')
